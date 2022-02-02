6bbaaa3be92b9512a3b97e170dbbbdbaaed241e1
Nick Sorros <nsorros@gmail.com>
2022-02-02 16:34:37 +0000

    Add test for spacy convert

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -1,6 +1,9 @@
 import json
 import os
 
+from spacy.tokens import DocBin
+import spacy
+
 from convert import convert
 
 
@@ -11,6 +14,14 @@ def read_jsonl(data_path):
             data.append(json.loads(line))
     return data
 
+def read_spacy(data_path):
+    docbin = DocBin()
+    docbin.from_disk(data_path)
+
+    nlp = spacy.blank("en")
+    docs = docbin.get_docs(nlp.vocab)
+    return docs
+
 def test_convert(tmp_path):
     input_path = os.path.join(os.path.dirname(__file__), "data/")
     output_path = tmp_path / "data.jsonl"
@@ -19,3 +30,12 @@ def test_convert(tmp_path):
 
     data = read_jsonl(output_path)
     assert len(data) == 12
+
+def test_convert_spacy(tmp_path):
+    input_path = os.path.join(os.path.dirname(__file__), "data/")
+    output_path = tmp_path / "data.spacy"
+    convert(input_path, output_path, format="spacy")
+    assert os.path.exists(output_path)
+
+    docs = read_spacy(output_path)
+    assert len(list(docs)) == 12
